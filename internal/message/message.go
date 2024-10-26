53ae5301e9364b5b0084fd5c104fca35d1191877
ArtyomKr <tyomik.krasnitsky@gmail.com>
2024-10-26 14:34:53 +0300

    feat: add state to spotify oauth calls

--- internal/message/message.go
+++ internal/message/message.go
@@ -2,13 +2,15 @@ package message
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
 	"telegram-bot/internal/spotify"
 )
 
 func Process(msg *tgbotapi.Message) tgbotapi.MessageConfig {
 	switch msg.Text {
 	case "/login":
-		return tgbotapi.NewMessage(msg.Chat.ID, spotify.GetLoginLink())
+		id := strconv.FormatInt(msg.From.ID, 10)
+		return tgbotapi.NewMessage(msg.Chat.ID, spotify.GetLoginLink(id))
 	}
 	return tgbotapi.NewMessage(msg.Chat.ID, "Unknown command")
 }
--- internal/spotify/helper.go
+++ internal/spotify/helper.go
@@ -1,20 +1,24 @@
 package spotify
 
 import (
-	"fmt"
 	"net/url"
 	"os"
 )
 
-func GetLoginLink() string {
+func GetLoginLink(state string) string {
 	const scopes = "user-read-playback-state user-modify-playback-state"
 	const adress = "https://accounts.spotify.com/authorize"
 	clientId := os.Getenv("SPOTIFY_CLIENT_ID")
 	redirectURI := os.Getenv("SPOTIFY_REDIRECT_URI")
 
-	query := fmt.Sprintf("?response_type=%s&client_id=%s&scope=%s&redirect_uri=%s",
-		url.QueryEscape("code"), url.QueryEscape(clientId), url.QueryEscape(scopes), url.QueryEscape(redirectURI))
+	query := url.Values{
+		"response_type": {"code"},
+		"client_id":     {clientId},
+		"scope":         {scopes},
+		"redirect_uri":  {redirectURI},
+		"state":         {state},
+	}
 
-	link := adress + query
+	link := adress + "?" + query.Encode()
 	return link
 }
