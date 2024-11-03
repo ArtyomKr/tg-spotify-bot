ca04bfd167b271730ee2104a39c42abc7bb6eca6
ArtyomKr <tyomik.krasnitsky@gmail.com>
2024-11-03 16:00:28 +0300

    feat: add http client property to spotify client

--- internal/spotify/client.go
+++ internal/spotify/client.go
@@ -1,9 +1,13 @@
 package spotify
 
-import "os"
+import (
+	"net/http"
+	"os"
+)
 
 func NewClient() *Client {
 	return &Client{
+		httpClient:   &http.Client{},
 		clientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
 		clientSecret: os.Getenv("SPOTIFY_CLIENT_SECRET"),
 		redirectURI:  os.Getenv("SPOTIFY_REDIRECT_URI"),
