81b6dc3a3bee18f9268d3c0ec0a6de984a5f38d2
ArtyomKr <tyomik.krasnitsky@gmail.com>
2024-11-09 15:42:21 +0300

    feat: add helpers to tg message handlers

--- internal/bot/handlers.go
+++ internal/bot/handlers.go
@@ -1,19 +1,38 @@
 package bot
 
 import (
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 	"strconv"
+	"strings"
 )
 
 func (b *Bot) handleMessage(msg *tgbotapi.Message) tgbotapi.MessageConfig {
-	switch msg.Text {
+	args := strings.Fields(msg.Text)
+	if len(args) == 0 {
+		return b.handleUnknownCommand(msg)
+	}
+
+	command := args[0]
+
+	switch command {
 	case "/login":
 		return b.handleLogin(msg)
 	case "/token":
 		return b.handleToken(msg)
 	case "/current":
 		return b.handleCurrent(msg)
+	case "/queue":
+		if len(args) < 2 {
+			return tgbotapi.NewMessage(msg.Chat.ID, "Please provide a URL. Usage: /queue <url>")
+		}
+		uri, ok := getSpotifyURI(args[1])
+		if !ok {
+			return tgbotapi.NewMessage(msg.Chat.ID, "URL is not a valid spotify track url")
+		}
 
+		return b.handleQueue(msg, uri)
 	default:
 		return b.handleUnknownCommand(msg)
 	}
@@ -47,12 +66,40 @@ func (b *Bot) handleCurrent(msg *tgbotapi.Message) tgbotapi.MessageConfig {
 
 	currentTrack, err := b.spotifyAPI.GetCurrentTrack(token)
 	if err != nil {
+		log.Printf("Req error: %v", err)
 		return tgbotapi.NewMessage(msg.Chat.ID, "Couldn't get playback status")
 	}
 
-	return tgbotapi.NewMessage(msg.Chat.ID, currentTrack.Item.Name)
+	response := fmt.Sprintf(
+		"⏸️ %s - %s\n⌛️Progress: %s\n%s",
+		currentTrack.Item.Name,
+		currentTrack.Item.Artists[0].Name,
+		formatPlaybackProgress(currentTrack.ProgressMs, currentTrack.Item.DurationMs),
+		currentTrack.Item.ExternalUrls.Spotify,
+	)
+	return tgbotapi.NewMessage(msg.Chat.ID, response)
+}
+
+func (b *Bot) handleQueue(msg *tgbotapi.Message, uri string) tgbotapi.MessageConfig {
+	id := strconv.FormatInt(msg.From.ID, 10)
+	token, err := b.spotifyAuth.GetToken(id)
+	if err != nil {
+		return tgbotapi.NewMessage(msg.Chat.ID, "Authorization failed")
+	}
+
+	err = b.spotifyAPI.AddTrackToQueue(token, uri)
+	if err != nil {
+		log.Printf("Req error: %v", err)
+		return tgbotapi.NewMessage(msg.Chat.ID, "Couldn't add track to queue")
+	}
+
+	return tgbotapi.NewMessage(msg.Chat.ID, "Added track to queue")
 }
 
 func (b *Bot) handleUnknownCommand(msg *tgbotapi.Message) tgbotapi.MessageConfig {
+	uri, ok := getSpotifyURI(msg.Text)
+	if ok {
+		return b.handleQueue(msg, uri)
+	}
 	return tgbotapi.NewMessage(msg.Chat.ID, "Unknown command")
 }
---
+++ internal/bot/helpers.go
@@ -0,0 +1,37 @@
+package bot
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var spotifyTrackPattern = regexp.MustCompile(`^(?:https?:\/\/)?(?:open\.)?spotify\.com\/track\/([a-zA-Z0-9]+)`)
+
+func formatDuration(ms int) string {
+	d := time.Duration(ms) * time.Millisecond
+	return fmt.Sprintf("%d:%02d", int(d.Minutes()), int(d.Seconds())%60)
+}
+
+func formatPlaybackProgress(progress, total int) string {
+	const barLength = 14
+	percentage := float64(progress) / float64(total)
+	completed := int(percentage * float64(barLength))
+	bar := strings.Repeat("─", completed) + "●" + strings.Repeat("━", barLength-completed)
+	return fmt.Sprintf("%s %s %s",
+		formatDuration(progress),
+		bar,
+		formatDuration(total),
+	)
+}
+
+func getSpotifyURI(url string) (string, bool) {
+	matches := spotifyTrackPattern.FindStringSubmatch(url)
+
+	if len(matches) < 2 {
+		return "", false
+	}
+
+	return "spotify:track:" + matches[1], true
+}
