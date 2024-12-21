a3a1c934931f4d2b36b56b139d9e4d5ef4858f47
ArtyomKr <tyomik.krasnitsky@gmail.com>
2024-12-21 22:21:42 +0300

    #3 | feat: add regex for album

--- internal/bot/helpers.go
+++ internal/bot/helpers.go
@@ -1,54 +1,46 @@
 package bot
 
 import (
-	"fmt"
 	"regexp"
-	"strings"
-	"time"
 )
 
 var spotifyTrackPattern = regexp.MustCompile(`(?:https?://)?(?:open\.)?spotify\.com/track/([a-zA-Z0-9]+)`)
+var spotifyAlbumPattern = regexp.MustCompile(`(?:https?://)?(?:open\.)?spotify\.com/album/([a-zA-Z0-9]+)`)
 
-func formatDuration(ms int) string {
-	d := time.Duration(ms) * time.Millisecond
-	return fmt.Sprintf("%d:%02d", int(d.Minutes()), int(d.Seconds())%60)
-}
+func getTrackIDsFromString(url string) []string {
+	matches := spotifyTrackPattern.FindAllStringSubmatch(url, -1)
 
-func formatPlaybackProgress(progress, total int) string {
-	const barLength = 20
-	percentage := float64(progress) / float64(total)
-	completed := int(percentage * float64(barLength))
+	if len(matches) > 0 {
+		trackIDs := make([]string, 0, len(matches))
 
-	bar := strings.Repeat("▰", completed) + strings.Repeat("▱", barLength-completed)
+		for _, match := range matches {
+			if len(match) >= 2 {
+				trackIDs = append(trackIDs, match[1])
+			}
+		}
 
-	// Add emoji indicators based on progress
-	var indicator string
-	switch {
-	case percentage < 0.33:
-		indicator = "⏳"
-	case percentage < 0.66:
-		indicator = "🎵"
-	default:
-		indicator = "🎶"
+		return trackIDs
 	}
 
-	return fmt.Sprintf("%s %s <code>%s</code> %s",
-		indicator,
-		formatDuration(progress),
-		bar,
-		formatDuration(total),
-	)
+	return []string{}
 }
 
-func getSpotifyURI(url string) []string {
-	matches := spotifyTrackPattern.FindAllStringSubmatch(url, -1)
-	URIs := make([]string, 0, len(matches))
+func getAlbumIDFromString(url string) string {
+	var ID string
 
-	for _, match := range matches {
-		if len(match) >= 2 {
-			URI := "spotify:track:" + match[1]
-			URIs = append(URIs, URI)
-		}
+	match := spotifyAlbumPattern.FindStringSubmatch(url)
+	if len(match) >= 2 {
+		ID = match[1]
+	}
+
+	return ID
+}
+
+func wrapTrackIdsIntoURIs(IDs []string) []string {
+	URIs := make([]string, len(IDs))
+
+	for i, ID := range IDs {
+		URIs[i] = "spotify:track:" + ID
 	}
 
 	return URIs
